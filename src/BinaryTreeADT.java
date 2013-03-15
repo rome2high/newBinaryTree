730dcf63729f7bf41540bdb600c810e528716e03
rome2high <mnsaigonese@gmail.com>
2013-03-14 22:50:01 -0700

    Update BinaryTreeADT

--- src/BinaryTreeADT.java
+++ src/BinaryTreeADT.java
@@ -1,6 +1,15 @@
 public interface BinaryTreeADT {
 	//finish the rest of this code unit.
-
-
-
+	public BTNode root();
+	public boolean isEmpty();
+	public int size();
+	public boolean isRoot(BTNode v);
+	public boolean hasParent(BTNode v);
+	public BTNode parent(BTNode v);
+	public boolean isLeaf(BTNode v);
+	public boolean hasLeft(BTNode v);
+	public BTNode left(BTNode v);
+	public boolean hasRight(BTNode v);
+	public BTNode right(BTNode v);
+	public void addNode(int value);
 }
